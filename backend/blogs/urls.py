d1c6772928ad6bef45817f9592e247ece9165a50
Vaishnavi Rajput <141741075+seduloushaze@users.noreply.github.com>
2024-06-23 11:35:04 +0530

    adding frontend

--- backend/blogs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns=[
-    path('', display_all),
-    path('<str:title>/', display_blog),
-    path('create/', create),
-    path('edit/', edit)
-]
